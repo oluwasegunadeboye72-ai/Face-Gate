6cb1be5dd765a283a27aced7b5a300beac7d9075
oluwasegunadeboye72-ai <oluwasegunadeboye72@gmail.com>
2025-10-28 11:18:00 +0100

    first commit

---
+++ Day Two/Java/FixedPrice.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class FixedPrice{
+    public static void main(String []args){
+
+	final double Fixed_pice = 50,000;
+
+	final double Depreciation_value = 8%
+
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Enter your Price");
+
+	int numberPrices = scanner.nextInt();
+        
+        double totalValue = FIXED_PRICE * numberItems;
+        
+	double threshold = 10000 * numberItems; 
+        
+	int years = 0;
+
+        while (totalValue > totalthreShold) {
+            
+	totalValue -= totalValue * DEPRECIATION_RATE;
+            
+	years++;
+        
+	}
+        
+         System.out.println("It will take " + years + " years for the value to drop below 10,000 per item.");
+
+       }
+
+   }
+
+}
+
+	
+
+    
+
+
+        
+  
+
+
+	
+
+	
+	
+
+
+
+
+
+
+
+}
+} 
